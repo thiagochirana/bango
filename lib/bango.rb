d57b681dd3ea4712c523d1b7711fd0998567e96f
ChiranaWSLArch <engsw.thiago@gmail.com>
2025-03-08 00:15:47 -0300

    feat: add new errors

--- lib/bango.rb
+++ lib/bango.rb
@@ -4,5 +4,16 @@ require_relative "bango/version"
 
 module Bango
   class Error < StandardError; end
-  # Your code goes here...
+
+  class TelephoneError < StandardError
+    def initialize
+      super(%(Telephone is wrong! It is a telephone number?))
+    end
+  end
+
+  class FormatError < StardardError
+    def initialize
+      super(%(Telephone has a wrong format!))
+    end
+  end
 end
