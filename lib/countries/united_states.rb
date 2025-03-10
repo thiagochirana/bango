e927707597127a1b537f0dc24816f127dd7147bf
Chirana <engsw.thiago@gmail.com>
2025-03-10 00:20:48 -0300

    feat: add United States

---
+++ lib/countries/united_states.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "base"
+module Bango
+  module Countries
+    class Unitedstates < Country
+      def self.valid?(phone)
+        !!(phone =~ /\A\+1\d{10}\z/)
+      end
+
+      def self.format(phone)
+        phone.gsub(/\A\+1(\d{3})(\d{3})(\d{4})\z/, "(\\1) \\2-\\3")
+      end
+
+      def self.generate
+        "+1#{rand(200..999)}#{rand(100..999)}#{rand(1000..9999)}"
+      end
+    end
+  end
+end
